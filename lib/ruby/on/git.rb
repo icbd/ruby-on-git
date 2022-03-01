3e861439c2fdd1b5c9742824f74520cd36304cf7
Baodong <wwwicbd@gmail.com>
2022-03-01 19:05:14 +0800

    Init project
    `bundle gem ruby-on-git`

---
+++ lib/ruby/on/git.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "git/version"
+
+module Ruby
+  module On
+    module Git
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
