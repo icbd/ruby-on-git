813fcf2b999c578e5e7667c870bd24b9ff3b9d55
Baodong <wwwicbd@gmail.com>
2022-03-02 20:40:37 +0800

    Refactor version

--- lib/ruby/on/git.rb
+++ lib/ruby/on/git.rb
@@ -4,13 +4,8 @@ require_relative "git/version"
 
 module Ruby
   module On
-    # Ruby::On::Git
     module Git
       class Error < StandardError; end
-
-      def self.version
-        VERSION
-      end
     end
   end
 end
