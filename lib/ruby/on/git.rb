609011a2c75a75ad93b1788344e4a878ae15a2e5
Baodong <wwwicbd@gmail.com>
2022-03-06 00:30:18 +0800

    Require nested rb files under git

--- lib/ruby/on/git.rb
+++ lib/ruby/on/git.rb
@@ -3,7 +3,7 @@
 require "active_support/all"
 require "byebug" if ENV["GEM_ENV"] == "development"
 
-Dir[File.join(__dir__, "git/*.rb")].sort.each { |f| require f }
+Dir[File.join(__dir__, "git/**/*.rb")].sort.each { |f| require f }
 
 module Ruby
   module On
