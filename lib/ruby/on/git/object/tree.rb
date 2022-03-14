4733340f32df1b382259666e03a11a6a5a43af3c
Baodong <wwwicbd@gmail.com>
2022-03-14 23:15:39 +0800

    Update tree.rb

--- lib/ruby/on/git/object/tree.rb
+++ lib/ruby/on/git/object/tree.rb
@@ -27,7 +27,7 @@ module Ruby
           end
 
           def file_frame(path)
-            blob = Blob.Where(path)
+            blob = Blob.where(path)
             ["100644 #{File.basename(path)}", blob.hash_id].pack("Z*H40")
           end
 
